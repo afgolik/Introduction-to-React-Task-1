8a06878e12435972372c32a26090c874bd0f7121
afgolik <64197556+afgolik@users.noreply.github.com>
2023-07-05 09:41:36 +0300

    переписал без использования jsx, только в этот раз правильно

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import './index.css';
+import { createElement } from "react";
 
-export const App = () => {
-	const app = document.createElement('div')
-	app.className = 'App'
-	document.body.append(app)
-	const appHeader = document.createElement('header')
-	appHeader.className = 'App-header'
-	app.append(appHeader)
-	const img = document.createElement('img')
-	img.className = 'App-logo'
-	img.src = logo
-	img.alt = "logo"
-	appHeader.append(img)
-	const about = document.createElement('p')
-	about.textContent = 'Home work'
-	appHeader.append(about)
-	const link = document.createElement('a')
-	link.className = 'App-link'
-	link.href = "https://reactjs.org"
-	link. target = "_blank"
-	link.rel = "noopener noreferrer"
-	link.textContent = 'Learn React'
-	appHeader.append(link)
-	const today = new Date().getFullYear()
-	const dateInHTML = document.createElement('p')
-	dateInHTML.append(today)
-	appHeader.append(dateInHTML)
-
-	return app
+function Greeting({ name }) {
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement(
+				'img',
+				{
+					className: 'App-logo',
+					src: logo,
+					alt: "logo",
+				},
+			),
+			createElement(
+				'p',
+				null,
+				'Home work',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+				'Learn React',
+			),
+			createElement(
+				'p',
+				null,
+				new Date().getFullYear()
+			)
+		)
+	)
 }
-//при запуске в браузере выскакивает ошибка на весь экран, но если ее закрыть, то внешне как при использовании jsx
+
+export function App() {
+	return createElement(
+		Greeting
+	)
+}
